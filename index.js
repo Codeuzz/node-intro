8cb2edb24c07b91ef2b05044bda3de1136c6a951
Codeuzz <belinda@louise.life>
2025-01-08 11:28:47 +0100

    api/muscs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import 'dotenv/config'
-import musics from "./data/musics.js";
 import cors from "cors"
+import musicsRouter from "./routes/musicsRouter.js";
 
 const app = express()
 const PORT = process.env.PORT || 8000
@@ -9,74 +9,11 @@ const PORT = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
+app.use('/api', musicsRouter)
 
-app.get("/", (request, response) => {
-    response.send('welcome to my API') 
+app.get('/', (request, response) => {
+    response.send(`Welcome to my API`)
 })
 
-app.get('/musics', (request, response) => {
-    response.json(musics)
-})
-
-
-app.get('/musics/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10)
-
-    try {
-        const music = musics.find(m => m.id === id); 
-        console.log(music)
-        if (!music) {
-            return res.status(404).json({ error: "Music not found" });
-        }
-
-        res.status(200).json(music)
-
-    }catch {
-        res.status(500).json({message: 'Internal server error'})
-
-    }
-    
-});
-
-
-app.post('/musics', (req, res) => {
-    let {name, author, genre} = req.body
-    try{
-        if(!name || !author || !genre){
-            return res.status(401).json({message : 'All fields are required'})
-        }
-        const newMusic = {
-            id : musics.length + 1,
-            name,
-            author,
-            genre
-        }
-        musics.push(newMusic)
-        return res.status(201).json(musics)
-    }
-    catch(err){
-        return res.status(500).json({ message : 'Internal server error'})
-    }
-})
-
-app.put('/musics/:id', (req, res) => {
-    const { id } = req.params
-    const { name, author, genre } = req.body;
-
-    try {
-        const musicIndex = musics.findIndex(music => music.id === parseInt(id));
-        if (musicIndex === -1) {
-            return res.status(404).json({ message: 'Music not found' });
-        }
-
-        if (name) musics[musicIndex].name = name;
-        if (author) musics[musicIndex].author = author
-        if (genre) musics[musicIndex].genre = genre
-
-        return res.status(200).json(musics[musicIndex])
-    } catch (err) {
-        return res.status(500).json({ message: 'Internal server error' });
-    }
-});
 
 app.listen(8000, () => console.log(`server running on port ${PORT}`))
--- routes/musicsRouter.js
+++ routes/musicsRouter.js
@@ -1,3 +1,74 @@
 import { Router } from "express";
+import musics from '../data/musics.js'
 
 const musicsRouter = Router()
+
+
+
+musicsRouter.get('/musics', (request, response) => {
+    response.json(musics)
+})
+
+
+musicsRouter.get('/musics/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+
+    try {
+        const music = musics.find(m => m.id === id); 
+        console.log(music)
+        if (!music) {
+            return res.status(404).json({ error: "Music not found" });
+        }
+
+        res.status(200).json(music)
+
+    }catch {
+        res.status(500).json({message: 'Internal server error'})
+
+    }
+    
+});
+
+
+musicsRouter.post('/musics', (req, res) => {
+    let {name, author, genre} = req.body
+    try{
+        if(!name || !author || !genre){
+            return res.status(401).json({message : 'All fields are required'})
+        }
+        const newMusic = {
+            id : musics.length + 1,
+            name,
+            author,
+            genre
+        }
+        musics.push(newMusic)
+        return res.status(201).json(musics)
+    }
+    catch(err){
+        return res.status(500).json({ message : 'Internal server error'})
+    }
+})
+
+musicsRouter.put('/musics/:id', (req, res) => {
+    const { id } = req.params
+    const { name, author, genre } = req.body;
+
+    try {
+        const musicIndex = musics.findIndex(music => music.id === parseInt(id));
+        if (musicIndex === -1) {
+            return res.status(404).json({ message: 'Music not found' });
+        }
+
+        if (name) musics[musicIndex].name = name;
+        if (author) musics[musicIndex].author = author
+        if (genre) musics[musicIndex].genre = genre
+
+        return res.status(200).json(musics[musicIndex])
+    } catch (err) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+export default musicsRouter
